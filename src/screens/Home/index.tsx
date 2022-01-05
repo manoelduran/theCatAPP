259bd726e3026819cfaeefe355f1a3e7f8c03874
Manoel Duran <manoel.duran@hotmail.com>
2022-01-05 17:42:42 -0300

    lista de gattos

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,13 +10,17 @@ import * as api from '../../services/api';
 import { SearchInput } from '../../components/SearchInput';
 
 export function Home() {
-    const [cats, setCats] = useState<Cat[]>([] as Cat[])
+    const [cats, setCats] = useState<Cat[]>([] as Cat[]);
+    const [loading, setLoading] = useState(true);
     async function ShowCats() {
         try {
             const catList = await api.SearchCats();
-            console.log(catList)
-        } catch(error: any){
-            return Alert.alert('Erro ao buscar a lista de gatos', error.message)
+            console.log(catList);
+            setCats(cats);
+        } catch (error: any) {
+            return Alert.alert('Erro ao buscar a lista de gatos', error.message);
+        } finally {
+            setLoading(false);
         }
     }
     return (
@@ -29,8 +33,8 @@ export function Home() {
             <Header>
                 <Title>CAT APP</Title>
             </Header>
-            <SearchInput/>
- 
+            <SearchInput />
+
         </Container>
     )
 }
