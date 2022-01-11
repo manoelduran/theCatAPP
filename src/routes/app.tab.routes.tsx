f6afb43bd19d873eb112c9160ceb2feb49c055d8
Manoel Duran <manoel.duran@hotmail.com>
2022-01-11 20:41:32 -0300

    estilizar pagina do gato

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -2,10 +2,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AppStackRoutes } from './app.stack.routes';
 import { MyCats } from '../screens/MyCats';
 import HomeSvg from '../assets/home.svg';
-import CarSvg from '../assets/car.svg';
-import ProfileSvg from '../assets/people.svg';
+import CatSvg from '../assets/cat.svg';
 import { Platform } from 'react-native';
-import theme from '../styles/theme';
 import { useTheme } from 'styled-components/native';
 
 const { Navigator, Screen } = createBottomTabNavigator();
@@ -37,7 +35,7 @@ export function AppTabRoutes() {
                 name="MyCats"
                 options={{
                     tabBarIcon: ({ focused, color }) => (
-                        <CarSvg width={24} height={24} fill={focused ? theme.colors.header : color} />
+                        <CatSvg width={24} height={24} fill={focused ? theme.colors.header : color} />
                     )
                 }}
                 component={MyCats}
